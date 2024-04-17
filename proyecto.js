ebe7cb9aad2abdf76dd6c998e32a0aa0ebdc5c54
gabrielamarante1 <153243893+gabrielamarante1@users.noreply.github.com>
2024-04-17 06:25:14 -0300

    Update proyecto.js

--- proyecto.js
+++ proyecto.js
@@ -1,6 +1,8 @@
-const contenidoTienda = document.getElementById("contenido-tienda")
-const verCarrito = document.getElementById("verCarrito")
-const contenedorModal = document.getElementById("contenedor-modal")
+const contenidoTienda = document.getElementById("contenido-tienda");
+const verCarrito = document.getElementById("verCarrito");
+const contenedorModal = document.getElementById("contenedor-modal");
+
+let carrito = JSON.parse(localStorage.getItem("carro")) || [];
 
 fetch("./db/db.json")
 .then((res) => res.json())
@@ -8,51 +10,54 @@ fetch("./db/db.json")
     const { productos } = data;
     console.log(data.productos);
     console.log(productos);
-    renderproductos(productos)
-});
 
-let carrito = JSON.parse(localStorage.getItem("carro")) || [];
-productos.forEach((producto)=> {
-    let contenidoProducto = document.createElement("div")
-    contenidoProducto.className = "tarjeta";
-    contenidoProducto.innerHTML = `
-    <img src="${producto.img}">
-    <h3>${producto.nombre}</h3>
-    <p class="precio">${producto.precio} $</p>
-    `;
-    contenidoTienda.append(contenidoProducto);
-    let botonComprar = document.createElement("button")
-    botonComprar.innerText = "Comprar";
-    botonComprar.className = "boton-comprar";
-    contenidoProducto.append(botonComprar);
+    productos.forEach((producto)=> {
+        let contenidoProducto = document.createElement("div")
+        contenidoProducto.className = "tarjeta";
+        contenidoProducto.innerHTML = `
+        <img src="${producto.img}">
+        <h3>${producto.nombre}</h3>
+        <p class="precio">${producto.precio} $</p>
+        `;
+        contenidoTienda.append(contenidoProducto);
+        let botonComprar = document.createElement("button")
+        botonComprar.innerText = "Comprar";
+        botonComprar.className = "boton-comprar";
+        contenidoProducto.append(botonComprar);
 
-    botonComprar.addEventListener("click", () => {
-    const repeat = carrito.some((repeatproduct) => repeatproduct.id === producto.id); 
-    if (repeat) {
-        carrito.forEach((prod) => {
-        if(prod.id === producto.id) {
-            prod.cantidad++;
-        }
-    });
-    } else {
-        
-        carrito.push({
-            id: producto.id,
-            img: producto.img,
-            nombre: producto.nombre,
-            precio: producto.precio,
-            cantidad: producto.cantidad,
+        botonComprar.addEventListener("click", () => {
+            const repeat = carrito.some((repeatproduct) => repeatproduct.id === producto.id); 
+            if (repeat) {
+                carrito.forEach((prod) => {
+                    if(prod.id === producto.id) {
+                        prod.cantidad++;
+                    }
+                });
+            } else {
+                carrito.push({
+                    id: producto.id,
+                    img: producto.img,
+                    nombre: producto.nombre,
+                    precio: producto.precio,
+                    cantidad: producto.cantidad,
+                });
+            }
+            savelocal();
         });
-        
-        }
-        savelocal();
     });
 })
+.catch((error) => {
+    console.error("Error fetching data:", error);
+});
 
 const savelocal = () => {
-localStorage.setItem("carro", JSON.stringify (carrito));
+    localStorage.setItem("carro", JSON.stringify (carrito));
 };
 
 JSON.parse(localStorage.getItem("carro"));
 
+// Function to render the cart could go here
 
+verCarrito.addEventListener("click", () => {
+    // Call the function to render the cart here
+});
